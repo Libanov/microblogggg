1b5c374b7705517b98e9ad420edaf3040751adf4
Sergei Libanov <libanovs@mail.ru>
2022-05-13 16:28:04 +0500

    q

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField \
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
     TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo \
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
     Length
 from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -33,4 +35,4 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit = SubmitField
+    submit = SubmitField('Submit')
